8cce23c804c2de47d1200f988cb4f12aed271f83
USER <USER@INET-00>
2017-10-24 19:17:29 +0900

    Commit

--- Chapter08/src/Problem_22.java
+++ Chapter08/src/Problem_22.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 public class Problem_22 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		char[] queue = new char[5];
+		int rear = 0;
+
+		char carName = 'A';
+		int select = 9;
+
+		while (select != 3) {
+			System.out.printf("<1> 넣기 <2> 빼기 <3> 종료");
+			select = s.nextInt();
+
+			switch (select) {
+			case 1:
+			case 2:
+			case 3:
+			default:
+
+			}
+		}
 
 	}
 
