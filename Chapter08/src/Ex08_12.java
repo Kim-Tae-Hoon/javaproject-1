aba4e825d72f0377bcae444f8c4f697c92a580ff
USER <USER@INET-00>
2017-10-24 18:11:12 +0900

    Commit

---
+++ Chapter08/src/Ex08_12.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ex08_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		char[] stack = new char[5];
+		int top = 0;
+
+		char carName = 'A';
+		int select = 9;
+
+		while (select != 3) {
+			System.out.printf("<1> ³Ö±â <2>»©±â <3>³¡ : ");
+			select = s.nextInt();
+			switch (select) {
+			case 1:
+			case 2:
+			case 3:
+			default:
+
+			}
+		}
+
+	}
+
+}
