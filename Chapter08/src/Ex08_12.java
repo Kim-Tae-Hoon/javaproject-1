74aa82d8c532d88a7da65e3dd1c702a4b9c21eaf
USER <USER@INET-00>
2017-10-24 18:38:45 +0900

    Commit

--- Chapter08/src/Ex08_12.java
+++ Chapter08/src/Ex08_12.java
@@ -14,12 +14,35 @@ public class Ex08_12 {
 		while (select != 3) {
 			System.out.printf("<1> 넣기 <2>빼기 <3>끝 : ");
 			select = s.nextInt();
+
 			switch (select) {
 			case 1:
-			case 2:
-			case 3:
-			default:
+				if (top >= 5) {
+					System.out.printf("터널에 꽉 차서 차가 못 들어감\n");
+				} else {
+					stack[top] = carName++;
+					System.out.printf("%c 자동차가 터널에 들어감\n", stack[top]);
+					top++;
+				}
+				break;
 
+			case 2:
+				if (top <= 0) {
+					System.out.printf("빠져나갈 자동차가 없음\n");
+				} else {
+					top--;
+					System.out.printf("%c 자동차가 터널에서 빠짐\n", stack[top]);
+					stack[top] = ' ';
+				}
+				break;
+
+			case 3:
+				System.out.printf("현재 터널에 %d대가 있음.\n", top);
+				System.out.printf("프로그램 종료\n");
+				break;
+
+			default:
+				System.out.printf("잘못 입력함. 다시 입력해라. \n");
 			}
 		}
 
