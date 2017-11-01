034000e276ccb88447688753542242c13887d8be
USER <USER@INET-00>
2017-11-01 18:31:01 +0900

    Commit

--- Chapter08/src/MineSweeper.java
+++ Chapter08/src/MineSweeper.java
@@ -7,7 +7,7 @@ public class MineSweeper {
 
 		for (int i = 0; i < 10; i++)
 			for (int j = 0; j < 10; j++)
-				if (Math.random() < 0.3)
+				if (Math.random() < 0.2)
 					board[i][j] = true;
 		for(int i=0;i<10;i++){
 			for(int j=0;j<10;j++)
